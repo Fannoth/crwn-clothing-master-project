b0d80a42d550ee0a72e20b4e03443a2b4f2016a8
Jakub Czucha <jakub.czucha@wp.pl>
2021-12-23 23:09:52 +0100

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import { Routes, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
