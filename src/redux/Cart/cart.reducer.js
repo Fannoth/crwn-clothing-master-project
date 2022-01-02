2892bc82abcce63dc86da67873e34f2f93d1d858
Jakub Czucha <jakub.czucha@wp.pl>
2022-01-02 22:30:06 +0100

    added addItem and removeItem functionaly to checkout component

--- src/redux/Cart/cart.reducer.js
+++ src/redux/Cart/cart.reducer.js
@@ -26,7 +26,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          (cartItem) => cartItem.id != action.payload.id
+          (cartItem) => cartItem.id !== action.payload.id
         ),
       };
     default:
