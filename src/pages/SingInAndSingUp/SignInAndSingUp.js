9b307bff81d0a79a46966c2a81a0f9d793105c66
Jakub Czucha <jakub.czucha@wp.pl>
2022-01-07 16:30:21 +0100

    added note for test signing in

--- src/pages/SingInAndSingUp/SignInAndSingUp.js
+++ src/pages/SingInAndSingUp/SignInAndSingUp.js
@@ -5,9 +5,18 @@ import "./SignInAndSingUp.scss";
 
 export const SignInAndSingUp = () => {
   return (
-    <div className="sign-in-and-sign-up">
-      <SignIn />
-      <SingUp />
+    <div className="wrapper">
+      <div className="sign-in-and-sign-up">
+        <SignIn />
+        <SingUp />
+      </div>
+      <span className="test-alert">
+        Please use following data for test login:
+        <br />
+        email: <strong>test123@gmail.com</strong>
+        <br />
+        password: <strong>test123</strong>
+      </span>
     </div>
   );
 };
