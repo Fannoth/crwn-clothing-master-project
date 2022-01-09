c971854fda6765927e94a5e6a00a57e50e194238
Jakub Czucha <jakub.czucha@wp.pl>
2022-01-09 16:04:12 +0100

    another fix logger access

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,10 @@ import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const middlewares = [logger];
+const middlewares = [];
 
-if (process.env.NODE_ENV === "production") {
-  middlewares.pop();
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
